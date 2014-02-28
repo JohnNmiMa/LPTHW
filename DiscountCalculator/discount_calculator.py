6ec9a69826dc1e8affd2f40e388d026ded64edf3
John <johnmarksjr@gmail.com>
2014-02-28 14:49:34 -0700

    added logic for percent and absolute discounts

--- DiscountCalculator/discount_calculator.py
+++ DiscountCalculator/discount_calculator.py
@@ -1,4 +1,9 @@
 class DiscountCalculator(object):
     def calculate(self, total, discount_amount, discount_type):
-        return 10.0
+        if discount_type == 'percent':
+            percentage_discount = float(discount_amount) / 100
+            discount = float(total) * percentage_discount
+        else:
+            discount = discount_amount
+        return discount
 
